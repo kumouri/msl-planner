7658dbb36f6b99f49294e98759b73822c4e25f81
Will Armstrong <will.c.armstrong@gmail.com>
2017-12-07 01:36:31 -0500

    AstromonStatistics added

--- src/main/java/com/kumouri/mslplanner/stats/AstromonStatistics.java
+++ src/main/java/com/kumouri/mslplanner/stats/AstromonStatistics.java
@@ -1,4 +1,13 @@
 package com.kumouri.mslplanner.stats;
 
+import lombok.Data;
+
+@Data
 public class AstromonStatistics {
+    private Integer baseHealth;
+    private Integer baseDefense;
+    private Integer baseAttack;
+    private Integer baseRecovery;
+    private Double baseCritRate;
+    private Double baseCritMulti;
 }
